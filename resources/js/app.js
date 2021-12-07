17e0cf56984bb3c2369d1ffb6fa652741c34f5fc
mudrahel <mudragelu@gmail.com>
2021-12-07 13:20:01 +0200

    Integrate vuejs to project && create RedirectDetectiveComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,14 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// Require Vue
+window.Vue = require('vue').default;
+
+// Register Vue Components
+Vue.component('redirect-detective-component', require('./components/RedirectDetectiveComponent.vue').default);
+
+// Initialize Vue
+const app = new Vue({
+    el: '#app',
+});
---
+++ resources/js/components/RedirectDetectiveComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="flex justify-center mt-20 mb-20 mx-auto w-3/4">
+        <div class="text-center p-4 sm:justify-between sm:p-12 bg-primary-100 rounded-md w-3/4">
+            <h1 class="text-4xl font-bold text-primary-500 mb-5">URL Redirect Checker</h1>
+            <div class="text-2xl font-semibold flex justify-center">
+                <input class="rounded-md px-4 focus:outline-none border border-gray-500 focus:border-primary-600 w-3/6" type="text" placeholder="https://domain.com">
+                <button class="bg-primary-600 hover:bg-primary-700 text-white rounded-md px-4 py-2 cursor-pointer ml-5">Trace URL</button>
+            </div>
+
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Example component mounted.')
+    }
+}
+</script>
